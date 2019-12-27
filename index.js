3f6c1e4b6e3b51ea672b20f9f93a4963dddaa46f
Jose Casado <jcasado6@yahoo.com>
2019-12-27 14:45:14 -0500

    routes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const breweriesRoutes = require('./lib/routes/breweries')
+const parser = require("body-parser");
+const cors = require('cors')
+
+app.use(parser.json());
+app.use(cors())
 
 app.listen(8080, () => console.log('listening'))
 
-app.use('/', breweriesRoutes)
+app.use('/breweries', breweriesRoutes)
+
