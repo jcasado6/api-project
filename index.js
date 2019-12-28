39ab74675de8374f99a7092aa63faaeb2cfbc865
Jose Casado <jcasado6@yahoo.com>
2019-12-28 13:06:25 -0500

    posting and updating somewhat

--- index.js
+++ index.js
@@ -3,9 +3,62 @@ const app = express()
 const breweriesRoutes = require('./lib/routes/breweries')
 const parser = require("body-parser");
 const cors = require('cors')
+const Breweries = require('./lib/models/Breweries')
+
 
 app.use(parser.json());
 app.use(cors())
+
+breweriesRoutes.route('/').get((req, res) => {
+    Breweries.find((err, Breweries) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(Breweries);
+        }
+    });
+});
+
+breweriesRoutes.route('/:id').get((req, res) => {
+    const id = req.params.id;
+    Breweries.findById(id, (err, Breweries) => {
+        res.json(Breweries);
+    });
+});
+
+breweriesRoutes.route('/add').post((req, res) => {
+    let breweries = new Breweries (req.body);
+    breweries.save()
+        .then(Breweries => {
+            res.status(200).json({'breweries': 'brewery added!'});
+        })
+        .catch(err => {
+            res.status(400).send('adding brewery failed');
+        });
+});
+
+breweriesRoutes.route('/update/:id').post((req, res) => {
+    Breweries.findById(req.params.id, (err, Breweries) => {
+        if (!Breweries)
+            res.status(404).send("not found");
+        else
+            Breweries.name = req.body.name;
+            Breweries.brewery_type = req.body.brewery_type;
+            Breweries.street = req.body.street;
+            Breweries.city = req.body.city;
+            Breweries.state = req.body.st;
+            Breweries.postal_code = req.body.postal_code;
+            Breweries.phone = req.body.phone;
+
+            Breweries.save().then(Breweries => {
+                res.json('brewery updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update failed");
+            });
+    });
+});
+
 
 app.listen(8080, () => console.log('listening'))
 
